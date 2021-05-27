822423fd1f04adc4120468a373025eaf659f1094
ryanstark23 <84939129+ryanstark23@users.noreply.github.com>
2021-05-28 05:03:54 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,54 @@
+<!-- Contact -->
+<h1>Contact Us</h1>
+  <div class="contact">
+    <div class="contact_container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="section_title text-center">
+              <div>Hello</div>
+              <h1>Say Hello</h1>
+            </div>
+            <div class="contact_text text-center">
+              <p>You can email on our website Forest View Resort</p><br/>
+            </div>
+            <div class="contact_form_container">
+           <form action="mail.php" method="post">
+    <label for="name">Your Name</label>
+    <input type="text"  name="name" placeholder="Your name..">
+    <label for="lname">Email</label>
+    <input type="email"  name="email" placeholder="Your email..">
+   
+    <label for="message">Message</label>
+    <textarea  name="message" placeholder="Write something.." style="height:200px"></textarea>
+    <input type="submit" value="Submit">
+  </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <div class="contact_map_container">
+      <div class="map">
+        <div id="google_map" class="google_map">
+          <div class="map_container">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Contact Map Content -->
+      <div class="contact_map_content">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+          <div class="contact_info_list">
+            <ul class="text-center">
+              <li>#1834 Bennette Road,Forestry, Olongapo, Zambales</li>
+              <li>1234-567-8900</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ thankyou.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>thank you</title>
+</head>
+<body>
+<h1> Theank you tol</h1>
+</body>
+</html>
